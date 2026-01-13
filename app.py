6c62f34b69bd52f934150fe2544e87001890c0bc
Zakia <zakiaelmanssouri18@gmail.com>
2026-01-13 21:06:41 +0100

    Add file logging

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-print("Hello AWS DevOps")
-print("Logging enabled")
+#!/usr/bin/env python3
+from datetime import datetime
+import os
+
+# créer le dossier logs s'il n'existe pas
+os.makedirs("logs", exist_ok=True)
+
+msg1 = "Hello AWS DevOps"
+msg2 = "Logging enabled"
+
+with open("logs/app.log", "a") as f:
+    f.write(f"{datetime.now().isoformat()} INFO {msg1}\n")
+    f.write(f"{datetime.now().isoformat()} INFO {msg2}\n")
+
+print(msg1)
+print(msg2)
+
